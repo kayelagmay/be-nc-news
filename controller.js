f593c59a03e3c8eaf86fd8f43aa406f22f4b897f
Kaye Lagmay <kayelagmay@icloud.com>
2024-11-25 15:32:01 +0000

    CORE: GET /api/topics

--- controller.js
+++ controller.js
@@ -1,5 +1,18 @@
 const endpointsJson = require("./endpoints.json");
+const { fetchAllTopics } = require('./model');
 
+// GET /api
 exports.getApi = (req, res) => {
     res.status(200).send({ endpoints: endpointsJson });
+};
+
+// GET /api/topics
+exports.getTopics = (req, res, next) => {
+    fetchAllTopics()
+      .then((topics) => {
+        res.status(200).send({ topics });
+      })
+      .catch((err) => {
+        next(err);
+      });
 };
