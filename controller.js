cfcf3703bb175c73d6f4ffdfdc31fd17e7192e86
Kaye Lagmay <kayelagmay@icloud.com>
2024-11-25 21:01:57 +0000

    Re-factored following feedback from 3-get-api-topics

--- controller.js
+++ controller.js
@@ -11,8 +11,5 @@ exports.getTopics = (req, res, next) => {
     fetchAllTopics()
       .then((topics) => {
         res.status(200).send({ topics });
-      })
-      .catch((err) => {
-        next(err);
       });
 };
