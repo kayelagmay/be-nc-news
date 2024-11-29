f88fed8f675fedc81f90637fe223772096eb9c9c
Kaye Lagmay <kayelagmay@icloud.com>
2024-11-29 12:51:35 +0000

    Added comment_count property to get /api/article/:article_id

--- model.js
+++ model.js
@@ -80,8 +80,12 @@ exports.fetchAllUsers = () => {
 
 exports.fetchArticleById = (article_id) => {
   return db
-  .query(`SELECT * FROM articles
-    WHERE article_id = $1;`,
+  .query(`SELECT articles.*, COUNT (comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`,
     [article_id])
   .then((result) => {
     if (result.rows.length === 0) {
